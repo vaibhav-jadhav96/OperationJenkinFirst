1e6d036a879d7b8a87468ec3ae61f6fb1758ec4a
vaibhav-jadhav96 <vaibhavjadhav3489>
2025-07-25 08:42:11 +0530

    Add project files.

---
+++ OperationJenkinFirst/Addition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationJenkinFirst
+{
+    internal class Addition
+    {
+        public void Add()
+        {
+            int a=10, b=20;
+            int c= a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ OperationJenkinFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationJenkinFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition a=new Addition();
+            a.Add();
+        }
+    }
+}
